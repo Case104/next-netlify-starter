e998fe7d38962b10c490975fa1756163e765a216
unknown <jpcase104@gmail.com>
2021-09-24 14:41:04 -0400

    Add sloths, change title, add posts

---
+++ components/Post.js
@@ -0,0 +1,3 @@
+export default function Post({ post }) {
+  return <h3>{post.frontmatter.title}</h3>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,53 @@
 import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
+import Post from "@components/Post";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container">
       <Head>
-        <title>case: 'BLOG' | jcase.dev</title>
+        <title>case 'BLOG': | jcase.dev</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <div>
+          {posts.map((post, idx) => {
+            return <Post post={post} key={`post-${idx}`} />;
+          })}
+        </div>
       </main>
 
       <Footer />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join("posts"));
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace(".md", "");
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8"
+    );
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
